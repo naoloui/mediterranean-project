e07630fc6238f65015d6b440ff2e999d5cee3ec4
naoloui <naomi.laufer16@gmail.com>
2025-09-10 17:30:34 +0200

    Social media icons added to the footer of the page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-function changeLanguage(lang) {
-  document.getElementById("myDropDown").classList.toggle("show");
-}
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
-export { changeLanguage };
+$("#myDropdown").on("show.bs.dropdown", function () {
+  // do something…
+});
