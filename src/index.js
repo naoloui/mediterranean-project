c3b6e848a1642e0fc9a4f10090ac6b5ccba79d14
naoloui <naomi.laufer16@gmail.com>
2025-10-16 11:38:44 +0200

    Card images added for Italy and Spain

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
   let slideIndex = 1;
   showSlides(slideIndex);
 
   function plusSlides(n) {
     showSlides((slideIndex += n));
   }
+};
 
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    {
       slideIndex = 1;
     }
-    if (n < 1) {
+  }
+  if (n < 1) {
+    {
       slideIndex = slides.length;
     }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
   }
-});
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
